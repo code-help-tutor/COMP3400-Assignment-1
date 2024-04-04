4f1deaa6e71a71be8abd9b6875a0b16ed6d88f97
Code Help Tutor <1823890830@qq.com>
2024-04-04 11:27:59 +0000

    WeChat cstutorcs COMP3400 Assignment 1

---
+++ PrincipalType.hs
@@ -0,0 +1,29 @@
+WeChat: cstutorcs
+QQ: 749389476
+Email: tutorcs@163.com
+module PrincipalType (f1, f2, f3, f4, f5, f5_inv) where
+-- Do NOT modify anything above this line.
+-- Do NOT import anything.
+
+{--
+Implement functions with the following types.
+The functions should be total and should not contain 'undefined' or 'error'.
+--}
+
+f1 :: (a -> b, a) -> b
+f1 = undefined
+
+f2 :: a -> (b, c) -> b
+f2 = undefined
+
+f3 :: (a -> a) -> a -> [a]
+f3 = undefined
+
+f4 :: (b -> r) -> (a -> b) -> (a -> r)
+f4 = undefined
+
+f5 :: ((a, b, c) -> d) -> a -> b -> c -> d
+f5 = undefined
+
+f5_inv ::  (a -> b -> c -> d) -> (a, b, c) -> d
+f5_inv = undefined
